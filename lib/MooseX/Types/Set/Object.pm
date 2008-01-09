b55b66cd3e2a30f6586ca613074d5b6d7850fcbb
Yuval Kogman <nothingmuch@woobling.org>
2008-01-09 18:46:35 +0000

    don't declare the Set type, just Set::Object

--- lib/MooseX/Types/Set/Object.pm
+++ lib/MooseX/Types/Set/Object.pm
@@ -11,24 +11,22 @@ our $VERSION = "0.01";
 use Set::Object ();
 use Scalar::Util ();
 
-use MooseX::Types -declare => [qw(Set)];
+use MooseX::Types;
 use MooseX::Types::Moose qw(Object ArrayRef);
 
-foreach my $type ( Set, "Set::Object" ) {
-	subtype $type,
-		as Object,
-		where { $_->isa("Set::Object") },
-		optimize_as \&optimized_check;
+subtype "Set::Object",
+	as Object,
+	where { $_->isa("Set::Object") },
+	optimize_as \&optimized_check;
 
 
-	coerce $type,
-		from ArrayRef,
-		via { Set::Object->new(@$_) };
+coerce "Set::Object",
+	from ArrayRef,
+	via { Set::Object->new(@$_) };
 
-	coerce ArrayRef,
-		from $type,
-		via { $_->members },
-}
+coerce ArrayRef,
+	from "Set::Object",
+	via { $_->members };
 
 sub optimized_check { Scalar::Util::blessed($_[0]) && $_[0]->isa("Set::Object") }
 
